7d43313e758be51daf601ca893b79feaacdb2568
Camden Withrow <withrowcamden@gmail.com>
2026-03-03 20:32:49 -0600

    basic grid

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,44 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+const DAYS = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+const habits = ["gym", "read"]
+
 app.get('/', (c) => {
   return c.html(
-    <div>Hello</div>
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      </head>
+      <body>
+        <div class="w-[90%] m-auto">
+          <h1 class="text-3xl font-bold underline">
+            Habit Tracker
+          </h1>
+          <div id="day-week-toggle" class=""></div>
+          <div class="">
+            <table>
+              <tr class="">
+                <th></th>
+                {DAYS.map(day => <th>{day}</th>)}
+              </tr>
+              {Array.from({ length: 12 }, (_, i) => (
+                <tr>
+                  <td>{i + 1}</td>
+                  {DAYS.map(_ => {
+                    habits.map(_ => (
+                      <input type="checkbox" />
+                    ))
+                  })}
+                </tr>
+              ))}
+            </table>
+          </div>
+        </div>
+      </body>
+    </html>
   )
 })
 
